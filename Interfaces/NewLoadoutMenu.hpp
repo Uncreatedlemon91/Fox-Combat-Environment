e6420cd9fd05352570287f8802e39432813dabe8
Uncreatedlemon91 <76181532+Uncreatedlemon91@users.noreply.github.com>
2023-06-17 13:43:50 -0400

    Update NewLoadoutMenu.hpp

--- Interfaces/NewLoadoutMenu.hpp
+++ Interfaces/NewLoadoutMenu.hpp
@@ -6,14 +6,6 @@ class NewLoadoutMenu
 	idd = 3;
 	class Controls
 	{
-		class IGUIBack_2200: IGUIBack
-		{
-			idc = 2200;
-			x = 0.0823437 * safezoneW + safezoneX;
-			y = 0.225 * safezoneH + safezoneY;
-			w = 0.185625 * safezoneW;
-			h = 0.55 * safezoneH;
-		};
 		class RscCombo_2100: RscCombo
 		{
 			idc = 2100;
@@ -38,14 +30,6 @@ class NewLoadoutMenu
 			y = 0.225 * safezoneH + safezoneY;
 			w = 0.134062 * safezoneW;
 			h = 0.44 * safezoneH;
-		};
-		class RscFrame_1800: RscFrame
-		{
-			idc = 1800;
-			x = 0.5825 * safezoneW + safezoneX;
-			y = 0.28 * safezoneH + safezoneY;
-			w = 0.118594 * safezoneW;
-			h = 0.484 * safezoneH;
 		};
 		class RscButtonMenu_2400: RscButtonMenu
 		{
